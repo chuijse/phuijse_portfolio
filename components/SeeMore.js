a2c350c3b2bdbbdb6a26a7d42267e3c1cda5bdcd
chuijse <cristian.huijse@gmail.com>
2022-10-14 18:35:38 -0300

    wip

--- components/SeeMore.js
+++ components/SeeMore.js
@@ -1,11 +1,14 @@
 import React from "react";
+import Link from "next/link";
 
 export default function SeeMore() {
   return (
     <React.Fragment>
-      <button className="see-more-root">
-        <code className="text">SeeMore</code>
-      </button>
+      <Link href="/">
+        <button className="see-more-root">
+          <a className="text">SeeMore</a>
+        </button>
+      </Link>
     </React.Fragment>
   );
 }
