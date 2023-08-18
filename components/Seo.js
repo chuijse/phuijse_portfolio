e80efd93faffb66f2b91970e88505088c1d67bac
chuijse <cristian.huijse@gmail.com>
2023-08-17 20:32:55 -0400

    new favicon

--- components/Seo.js
+++ components/Seo.js
@@ -4,10 +4,19 @@ import { useRouter } from "next/router";
 export default function Seo({
   pageTitle = "Pablo Huijse's personal website",
   description = "Pablo Huijse is an assistant professor at Universidad Austral de Chile and young researcher at the Millennium Institute of Astrophysics. His research is on machine learning and data science with applications in astronomy and engineering.",
-  icon = "/favicon.ico",
+
   icon16 = "/favicon-16x16.png",
   icon32 = "/favicon-32x32.png",
-  iconApple = "/apple-touch-icon.png",
+  icon96 = "/favicon-96x96.png",
+  icon192 = "/android-icon-192x192.png",
+  iconApple57 = "/apple-icon-57x57.png",
+  iconApple60 = "/apple-icon-60x60.png",
+  iconApple72 = "/apple-icon-72x72.png",
+  iconApple76 = "/apple-icon-76x76.png",
+  iconApple114 = "/apple-icon-114x114.png",
+  iconApple144 = "/apple-icon-144x144.png",
+  iconApple152 = "/apple-icon-152x152.png",
+  iconApple180 = "/apple-icon-180x180.png",
   image = "/meta-data.jpg",
   year = "2023",
   author = "Cristian Huijse Heise",
@@ -66,11 +75,19 @@ export default function Seo({
       <meta name="reply-to" content="pablo.huijse@gmail.com" />
       <link rev="made" href="mailto:pablo.huijse@gmail.com" />
       {/*icons*/}
-      <link rel="shortcut icon" href={icon} />
-      <link rel="apple-touch-icon" sizes="180x180" href={iconApple} />
+      <link rel="apple-touch-icon" sizes="57x57" href={iconApple57} />
+      <link rel="apple-touch-icon" sizes="60x60" href={iconApple60} />
+      <link rel="apple-touch-icon" sizes="72x72" href={iconApple72} />
+      <link rel="apple-touch-icon" sizes="76x76" href={iconApple76} />
+      <link rel="apple-touch-icon" sizes="114x114" href={iconApple114} />
+      <link rel="apple-touch-icon" sizes="144x144" href={iconApple144} />
+      <link rel="apple-touch-icon" sizes="152x152" href={iconApple152} />
+      <link rel="apple-touch-icon" sizes="180x180" href={iconApple180} />
       <link rel="icon" type="image/png" sizes="32x32" href={icon32} />
       <link rel="icon" type="image/png" sizes="16x16" href={icon16} />
-      <link rel="manifest" href="/site.webmanifest"></link>
+      <link rel="icon" type="image/png" sizes="96x96" href={icon96} />
+      <link rel="icon" type="image/png" sizes="192x192" href={icon192} />
+      <link rel="manifest" href="/manifest.json" />
       {/*<meta name="theme-color" content="#ffffff" />*/}
     </Head>
   );
